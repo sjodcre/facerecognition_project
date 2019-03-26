f7450081b0b92952a659562e04f2fd52b0c89d74
Samuel Omamalin <samuelomamalin@gmail.com>
2019-03-26 01:48:49 -0400

    apply facerecognition api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import Clarifai from 'clarifai';
 import './App.css';
 import Particles from 'react-particles-js';
+
+const app = new Clarifai.App({
+ apiKey: '2a4557c268a74374a3c3f700e4c106bd'
+});
 
 const particlesOptions = { 
   "particles": {
@@ -119,6 +125,48 @@ const particlesOptions = {
 }
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      input:'',
+      imageUrl: '',
+      box:{},
+    }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace =data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    console.log(width,height);
+    return {
+      leftCol : clarifaiFace.left_col *width,
+      topRow : clarifaiFace.top_row *height,
+      rightCol : width - (clarifaiFace.right_col*width),
+      bottomRow: height - (clarifaiFace.bottom_row*height),
+    }
+  }
+
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({box:box});
+  }
+
+  onInputChange = (event) => {
+    this.setState({input:event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl:this.state.input});
+    app.models
+      .predict(
+        Clarifai.FACE_DETECT_MODEL,
+        this.state.input)
+      .then(response=> this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch(err=>console.log(err));
+  }
+
   render() {
     return (
       <div className="App">
@@ -127,8 +175,11 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
-        
+        <ImageLinkForm 
+        onInputChange={this.onInputChange} 
+        onButtonSubmit={this.onButtonSubmit} 
+        />
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
       </div>
     );
   }
